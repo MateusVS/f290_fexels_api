09a9ad8fc9140f6397164a087ee91bc316e7b55a
MateusVS <mateusviniciussilva07@gmail.com>
2020-05-07 20:32:37 -0300

    Criacao da home page do app em screens/home_page.dart

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pexels Search"),
+        backgroundColor: Color(0xFF232A34),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(child: Text('Scaffold')),
+        ],
+      ),
+    );
+  }
+}
