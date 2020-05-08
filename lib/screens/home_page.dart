081a516cf72cc82a2cb0c2fc21174157b37a40b3
MateusVS <mateusviniciussilva07@gmail.com>
2020-05-07 21:58:50 -0300

    Inicio criacao UI

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as json;
@@ -12,27 +14,70 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() async {
     super.initState();
-    obterDadosPokeAPI();
+    _getImages().then((value) => print(value));
   }
 
-  obterDadosPokeAPI() async {
-    http.Response response = await http.get('https://pokeapi.co/api/v2/pokemon/pikachu');
-    print(response.body);
+  static const String API_KEY = '563492ad6f917000010000018cea5845c8354912920099138687a563';
+
+  final url = 'https://api.pexels.com/v1/curated?per_page=20&page=1';
+
+  Future _getImages() async {
+    http.Response response = await http.get(url, headers: {
+      HttpHeaders.authorizationHeader: API_KEY
+    });
+
+    if (response.statusCode == 200) {
+      return json.jsonDecode(response.body);
+    } else {
+      print("Erro ao obter imagens. Status Code ${response.statusCode}");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Pexels Search"),
-        backgroundColor: Color(0xFF232A34),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(child: Text('Scaffold')),
-        ],
-      ),
+      backgroundColor: Color(0xFF232A34),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Color(0xFF232A34),
+              padding: EdgeInsets.all(5),
+              child: Row(
+                children: <Widget>[
+                  Image.network(
+                    'http://images.pexel.com/lib/api/pexels-white.png',
+                    height: 35,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 1),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.red,
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
